ce718aa5705a8844f8dd453b498c8d1e539974b5
nasvalljohan <j.nasvall95@gmail.com>
2022-04-13 09:43:45 +0200

    Calender function moved to viewmodel
    Co-Authored-By: oskla <90769758+oskla@users.noreply.github.com>

--- app/src/main/java/com/antisnusbolaget/slutasnusa2/DateFragment.kt
+++ app/src/main/java/com/antisnusbolaget/slutasnusa2/DateFragment.kt
@@ -5,23 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.NavController
-import androidx.navigation.fragment.FragmentNavigator
 import androidx.navigation.fragment.findNavController
 import com.antisnusbolaget.slutasnusa2.databinding.FragmentDateBinding
 import com.antisnusbolaget.slutasnusa2.model.UserViewModel
-import com.google.android.material.datepicker.MaterialDatePicker
-import kotlinx.coroutines.selects.select
-import java.text.SimpleDateFormat
-import java.time.Instant
-import java.util.*
-import kotlin.time.Duration.Companion.days
 
 
 class DateFragment : Fragment() {
+
 
     private val sharedViewModel: UserViewModel by activityViewModels()
     private var binding: FragmentDateBinding? = null
@@ -37,7 +28,7 @@ class DateFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         // Inflate the layout for this fragment
         val fragmentBinding = FragmentDateBinding.inflate(inflater, container, false)
@@ -50,32 +41,18 @@ class DateFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
 
-        val datePicker = MaterialDatePicker.Builder.datePicker()
-            .setTitleText("Start date")
-            .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
-            .build()
+
 
 
         binding?.apply {
 
-
-
             btnTest.setOnClickListener {
-                datePicker.show(childFragmentManager,"tag")
-                datePicker.addOnPositiveButtonClickListener {
-                    val dateFormatter = SimpleDateFormat("dd-MM-yyyy")
-                    val date = dateFormatter.format(Date(it))
-                    Toast.makeText(context, "$date is selected", Toast.LENGTH_LONG).show()
+                val manager = childFragmentManager
+                sharedViewModel.calenderSelection(manager)
 
-                    println(date)
-                    sharedViewModel.setDate(date)
-                    findNavController().navigate(R.id.action_dateFragment_to_homeFragment)
-
+                    //findNavController().navigate(R.id.action_dateFragment_to_homeFragment)
                 }
-
             }
-
         }
     }
 
-}
--- app/src/main/java/com/antisnusbolaget/slutasnusa2/model/UserViewModel.kt
+++ app/src/main/java/com/antisnusbolaget/slutasnusa2/model/UserViewModel.kt
@@ -1,15 +1,25 @@
 package com.antisnusbolaget.slutasnusa2.model
 
+import android.widget.Toast
+import androidx.fragment.app.FragmentContainer
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.text.SimpleDateFormat
 import java.util.*
-
-
+import kotlin.coroutines.coroutineContext
 
 
 class UserViewModel : ViewModel() {
+
+    // CalenderBuilder
+    private val datePicker = MaterialDatePicker.Builder.datePicker()
+        .setTitleText("Start date")
+        .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+        .build()
 
     private val _unitPerWeek = MutableLiveData<Int>(10)
     val unitPerWeek: LiveData<Int> = _unitPerWeek
@@ -31,6 +41,19 @@ class UserViewModel : ViewModel() {
     fun setDate(quitDate: String) {
         _quitDate.value = quitDate
     }
+
+
+    fun calenderSelection(manager: FragmentManager) {
+        datePicker.show(manager, "tag")
+        datePicker.addOnPositiveButtonClickListener {
+            val dateFormatter = SimpleDateFormat("dd-MM-yyyy")
+            val date = dateFormatter.format(Date(it))
+
+            println(date)
+            setDate(date)
+        }
+    }
+
 
 
 }
