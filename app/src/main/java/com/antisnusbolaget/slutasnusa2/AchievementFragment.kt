932fb390d0c91ee52523f7b694ca5b717afea9ae
nasvalljohan <j.nasvall95@gmail.com>
2022-04-28 20:47:26 +0200

    ViewPager2 laid out, bugs to be handled
    Co-Authored-By: oskla <90769758+oskla@users.noreply.github.com>

--- app/src/main/java/com/antisnusbolaget/slutasnusa2/AchievementFragment.kt
+++ app/src/main/java/com/antisnusbolaget/slutasnusa2/AchievementFragment.kt
@@ -1,17 +1,23 @@
 package com.antisnusbolaget.slutasnusa2
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.annotation.IdRes
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.NavController
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.antisnusbolaget.slutasnusa2.databinding.FragmentAchievementBinding
 import com.antisnusbolaget.slutasnusa2.model.UserViewModel
 
 class AchievementFragment : Fragment() {
+    private val NUM_PAGES = 5
+    private lateinit var viewPager: ViewPager2
     private val sharedViewModel: UserViewModel by activityViewModels()
     private var binding: FragmentAchievementBinding? = null
 
@@ -31,9 +37,31 @@ class AchievementFragment : Fragment() {
         // Inflate the layout for this fragment
         val fragmentBinding = FragmentAchievementBinding.inflate(inflater, container, false)
         binding = fragmentBinding
+
+        viewPager = binding!!.root.findViewById(R.id.pager)
+
+        val pagerAdapter = ScreenSlidePagerAdapter(this)
+        viewPager.adapter = pagerAdapter
+
+
+
         return fragmentBinding.root
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        val callback: OnBackPressedCallback =
+            object : OnBackPressedCallback(true)
+            {
+                override fun handleOnBackPressed() {
+                    println("cannot go back? xD ")
+                }
+            }
+        requireActivity().onBackPressedDispatcher.addCallback(
+            this,
+            callback
+        )
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -41,4 +69,10 @@ class AchievementFragment : Fragment() {
 
         }
     }
+    private inner class ScreenSlidePagerAdapter(fa: AchievementFragment) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = NUM_PAGES
+
+        override fun createFragment(position: Int): Fragment = ScreenSlidePageFragment()
+    }
+
 }
---
+++ app/src/main/java/com/antisnusbolaget/slutasnusa2/ScreenSlidePageFragment.kt
@@ -0,0 +1,46 @@
+package com.antisnusbolaget.slutasnusa2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.IdRes
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.NavController
+import com.antisnusbolaget.slutasnusa2.databinding.FragmentAchievementBinding
+import com.antisnusbolaget.slutasnusa2.databinding.FragmentScreenSlidePageBinding
+import com.antisnusbolaget.slutasnusa2.model.UserViewModel
+
+
+class ScreenSlidePageFragment : Fragment() {
+    private val sharedViewModel: UserViewModel by activityViewModels()
+    private var binding: FragmentScreenSlidePageBinding? = null
+
+    // Prevents multiple navController calls
+    private fun NavController.safelyNavigate(@IdRes resId: Int, args: Bundle? = null) =
+        try { navigate(resId, args) }
+        catch (e: Exception) { (e) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        val fragmentBinding = FragmentScreenSlidePageBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+
+        }
+    }
+}
