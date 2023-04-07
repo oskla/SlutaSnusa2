d27e036460df0d61d322a537fb5e9e13f7ac14b6
nasvalljohan <j.nasvall95@gmail.com>
2023-04-07 16:31:53 +0200

    Screens added

---
+++ app/src/main/java/com/antisnusbolaget/slutasnusa2/ui/screens/DateScreen.kt
@@ -0,0 +1,35 @@
+package com.antisnusbolaget.slutasnusa2.ui.screens
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun DateScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Gray),
+    ) {
+        Text(text = "DateScreen", fontSize = 30.sp)
+    }
+}
+
+private const val componentName = "DateScreen"
+
+@Preview("$componentName (light)", showBackground = true)
+@Preview("$componentName (dark)", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview("$componentName (big font)", fontScale = 1.5f, showBackground = true)
+@Preview("$componentName (large screen)", device = Devices.PIXEL_C)
+@Composable
+private fun PreviewComponent() {
+    DateScreen()
+}
