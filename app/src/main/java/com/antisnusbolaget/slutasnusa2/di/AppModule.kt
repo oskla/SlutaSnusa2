83bbca1311a83723167f682410b72f0bddd89c22
nasvalljohan <j.nasvall95@gmail.com>
2023-04-07 13:14:44 +0200

    Refactor

--- app/src/main/java/com/antisnusbolaget/slutasnusa2/di/AppModule.kt
+++ app/src/main/java/com/antisnusbolaget/slutasnusa2/di/AppModule.kt
@@ -4,6 +4,6 @@ import com.antisnusbolaget.slutasnusa2.model.UserViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val AppModule = module {
+val viewModel = module {
     viewModel { UserViewModel(get()) }
 }
