ddc1dc35cb38c400f43484a46fac3dfa92698cbe
nasvalljohan <j.nasvall95@gmail.com>
2023-04-09 14:14:52 +0200

    Change to svg instead of text-element Add review changes

--- app/src/main/java/com/antisnusbolaget/slutasnusa2/ui/screens/UnitScreen.kt
+++ app/src/main/java/com/antisnusbolaget/slutasnusa2/ui/screens/UnitScreen.kt
@@ -7,21 +7,25 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.antisnusbolaget.slutasnusa2.ui.theme.MontserratFamily
+import com.antisnusbolaget.slutasnusa2.R
 import com.antisnusbolaget.slutasnusa2.ui.theme.SlutaSnutaTheme
 import com.antisnusbolaget.slutasnusa2.viewmodel.OnBoardingViewModel
 import org.koin.androidx.compose.koinViewModel
@@ -36,42 +40,44 @@ fun UnitScreenContent() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background),
+            .background(MaterialTheme.colorScheme.background)
+            .padding(horizontal = 16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceEvenly,
+        verticalArrangement = Arrangement.SpaceEvenly.also { Arrangement.spacedBy(space = 30.dp) },
     ) {
         Text(
             text = "Hur många dosor snusar du i veckan?",
             fontSize = 40.sp,
-            //fontFamily = MontserratFamily,
             fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center,
             modifier = Modifier.width(320.dp),
+            minLines = 3,
         )
 
         Text(
             text = "0", // Placeholder
             fontSize = 100.sp,
-            //fontFamily = MontserratFamily,
             fontWeight = FontWeight.Bold,
         )
 
-        Row {
-            IconButton(onClick = { println("Increment") }) {
-                Text(
-                    text = "-",
-                    fontSize = 115.sp,
-                    //fontFamily = MontserratFamily,
-                    fontWeight = FontWeight.Bold,
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            IconButton(onClick = { println("Decrement") }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.icon_decrement),
+                    contentDescription = "Remove unit",
+                    modifier = Modifier.size(80.dp),
                 )
             }
-            Spacer(Modifier.size(29.dp))
-            IconButton(onClick = { println("Decrement") }) {
-                Text(
-                    text = "+",
-                    fontSize = 115.sp,
-                    //fontFamily = MontserratFamily,
-                    fontWeight = FontWeight.Bold,
+            Spacer(Modifier.width(29.dp))
+            IconButton(onClick = { println("Increment") }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.icon_increment),
+                    contentDescription = "Add unit",
+                    modifier = Modifier.size(80.dp),
                 )
             }
         }
