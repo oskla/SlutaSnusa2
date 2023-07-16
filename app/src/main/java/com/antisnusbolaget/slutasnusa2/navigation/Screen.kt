cb517595afaa00de12903f10bf88aec1724ff960
nasvalljohan <j.nasvall95@gmail.com>
2023-07-16 21:07:43 +0200

    Refactor names

--- app/src/main/java/com/antisnusbolaget/slutasnusa2/navigation/Screen.kt
+++ app/src/main/java/com/antisnusbolaget/slutasnusa2/navigation/Screen.kt
@@ -1,6 +1,10 @@
 package com.antisnusbolaget.slutasnusa2.navigation
 
-data class BottomBarVisibility(val shouldShowNav: Boolean, val shouldShowYellow: Boolean)
+import androidx.activity.OnBackPressedCallback
+import androidx.compose.runtime.mutableStateOf
+
+data class BottomBarVisibility(val isHomeScreen: Boolean, val isOnBoarding: Boolean)
+
 sealed class Screen(val route: String, var title: String) {
     object Home : Screen(route = "home_screen", title = "Home")
     object Settings : Screen(route = "settings_screen", title = "Settings")
@@ -10,6 +14,35 @@ sealed class Screen(val route: String, var title: String) {
     object Achievement : Screen(route = "achievement_screen", title = "Achievement")
 
     companion object {
+
+        private val onBoardingScreensIndex = mutableStateOf(0)
+
+        private fun handleBackAndForwardNavigation(): String {
+            return when (onBoardingScreensIndex.value) {
+                0 -> Cost.route
+                1 -> Unit.route
+                2 -> Date.route
+                else -> Home.route
+            }
+        }
+
+        fun onBackPressed(moveToBack: OnBackPressedCallback): String {
+            if (onBoardingScreensIndex.value <= 0) {
+                onBoardingScreensIndex.value = 0 // för att säkerställa att det verkligen aldrig blir mindre än 0
+
+                moveToBack.handleOnBackPressed()
+            }
+
+            onBoardingScreensIndex.value = onBoardingScreensIndex.value - 1
+
+            return handleBackAndForwardNavigation()
+        }
+
+        fun nextScreen(): String {
+            onBoardingScreensIndex.value += 1
+            return handleBackAndForwardNavigation()
+        }
+
         private val screensWithTopBar = listOf(
             Cost.route,
             Unit.route,
@@ -33,10 +66,16 @@ sealed class Screen(val route: String, var title: String) {
                 Date.route,
             )
 
-            if (route.isNullOrBlank()) return BottomBarVisibility(shouldShowNav = false, shouldShowYellow = false)
+            if (route.isNullOrBlank()) {
+                return BottomBarVisibility(
+                    isHomeScreen = false,
+                    isOnBoarding = false,
+                )
+            }
 
             val showBottomNav = screensWithBottomNav.any { route.contains(it, ignoreCase = true) }
-            val showBottomYellow = screensWithBottomYellow.any { route.contains(it, ignoreCase = true) }
+            val showBottomYellow =
+                screensWithBottomYellow.any { route.contains(it, ignoreCase = true) }
 
             return BottomBarVisibility(showBottomNav, showBottomYellow)
         }
