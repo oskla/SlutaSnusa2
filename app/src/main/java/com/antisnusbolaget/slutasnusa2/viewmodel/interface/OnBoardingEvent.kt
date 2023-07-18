0b3b6475aca4ac9bd555da1653c78fcf58c645f8
nasvalljohan <j.nasvall95@gmail.com>
2023-07-18 20:04:21 +0200

    Add dataclass for state

--- app/src/main/java/com/antisnusbolaget/slutasnusa2/viewmodel/interface/OnBoardingEvent.kt
+++ app/src/main/java/com/antisnusbolaget/slutasnusa2/viewmodel/interface/OnBoardingEvent.kt
@@ -5,3 +5,9 @@ sealed interface OnBoardingEvent {
     data class SetUnit(val unitAmount: Int) : OnBoardingEvent
     data class SetDate(val date: Long) : OnBoardingEvent
 }
+
+data class OnBoardingState(
+    val costPerUnit: Int = 0,
+    val amountOfUnits: Int = 0,
+    val dateWhenQuick: Long = 0L,
+)
