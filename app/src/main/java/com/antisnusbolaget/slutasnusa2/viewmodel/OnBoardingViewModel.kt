1ffe9ad646d26479613ebc5ef8141e2518a983b7
nasvalljohan <j.nasvall95@gmail.com>
2023-07-18 20:05:16 +0200

    Implement state in viewmodel

--- app/src/main/java/com/antisnusbolaget/slutasnusa2/viewmodel/OnBoardingViewModel.kt
+++ app/src/main/java/com/antisnusbolaget/slutasnusa2/viewmodel/OnBoardingViewModel.kt
@@ -3,16 +3,12 @@ package com.antisnusbolaget.slutasnusa2.viewmodel
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.antisnusbolaget.slutasnusa2.viewmodel.`interface`.OnBoardingEvent
+import com.antisnusbolaget.slutasnusa2.viewmodel.`interface`.OnBoardingState
 import java.util.concurrent.TimeUnit
 
 class OnBoardingViewModel : ViewModel() {
 
-    var costPerUnit = mutableStateOf(0)
-        private set
-    var amountOfUnits = mutableStateOf(0)
-        private set
-    var dateWhenQuit = mutableStateOf(0L)
-        private set
+    val state = mutableStateOf(OnBoardingState())
 
     fun handleEvents(event: OnBoardingEvent) {
         when (event) {
@@ -23,16 +19,15 @@ class OnBoardingViewModel : ViewModel() {
     }
 
     private fun setCostPerUnit(action: OnBoardingEvent.SetCost) {
-        costPerUnit.value = action.cost
+        state.value = state.value.copy(costPerUnit = action.cost)
     }
 
     private fun setUnit(action: OnBoardingEvent.SetUnit) {
-        amountOfUnits.value = action.unitAmount
+        state.value = state.value.copy(amountOfUnits = action.unitAmount)
     }
 
     private fun formatDate(action: OnBoardingEvent.SetDate) {
-        dateWhenQuit.value = action.date
-        println(dateWhenQuit.value)
+        state.value = state.value.copy(dateWhenQuick = action.date)
         val date1millis = System.currentTimeMillis()
 
         val days = TimeUnit.MILLISECONDS.toDays(date1millis - action.date)
