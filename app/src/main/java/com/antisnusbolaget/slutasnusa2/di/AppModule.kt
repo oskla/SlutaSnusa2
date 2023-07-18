ef1943d1cfeefbcbbe2858051724a5517806608f
nasvalljohan <j.nasvall95@gmail.com>
2023-07-18 20:03:18 +0200

    Lint

--- app/src/main/java/com/antisnusbolaget/slutasnusa2/di/AppModule.kt
+++ app/src/main/java/com/antisnusbolaget/slutasnusa2/di/AppModule.kt
@@ -1,9 +1,9 @@
 package com.antisnusbolaget.slutasnusa2.di
 
+import com.antisnusbolaget.slutasnusa2.old.viewmodel.UserViewModel
 import com.antisnusbolaget.slutasnusa2.viewmodel.AchievementViewModel
 import com.antisnusbolaget.slutasnusa2.viewmodel.HomeScreenViewModel
 import com.antisnusbolaget.slutasnusa2.viewmodel.OnBoardingViewModel
-import com.antisnusbolaget.slutasnusa2.old.viewmodel.UserViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
