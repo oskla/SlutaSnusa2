ce843493997625f6a2375d70960fb940f6f26a59
Oskar Larsson <90769758+oskla@users.noreply.github.com>
2023-07-25 14:05:11 +0200

    Add functionality for setting quitDate

--- app/src/main/java/com/antisnusbolaget/slutasnusa2/viewmodel/OnBoardingViewModel.kt
+++ app/src/main/java/com/antisnusbolaget/slutasnusa2/viewmodel/OnBoardingViewModel.kt
@@ -1,12 +1,13 @@
 package com.antisnusbolaget.slutasnusa2.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.antisnusbolaget.slutasnusa2.ui.screens.onboardingscreen.OnBoardingHelpers
 import com.antisnusbolaget.slutasnusa2.viewmodel.`interface`.OnBoardingEvent
 import com.antisnusbolaget.slutasnusa2.viewmodel.`interface`.OnBoardingNavigationView
 import com.antisnusbolaget.slutasnusa2.viewmodel.`interface`.UserData
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import java.util.concurrent.TimeUnit
+import timber.log.Timber
 
 class OnBoardingViewModel : ViewModel() {
 
@@ -21,11 +22,20 @@ class OnBoardingViewModel : ViewModel() {
     fun handleEvents(event: OnBoardingEvent) {
         when (event) {
             is OnBoardingEvent.SetCost -> setCostPerUnit(event.cost)
-            is OnBoardingEvent.SetDate -> formatDate(event.date)
+            is OnBoardingEvent.SetDate -> setQuitDate(event.date)
             is OnBoardingEvent.SetUnit -> setUnit(event.unitAmount)
 
             is OnBoardingEvent.NavigateToNextView -> navigateToNextView()
             // TODO handle backpress/swipe
+            OnBoardingEvent.DismissCalendar -> showCalendar(false)
+            OnBoardingEvent.OpenCalendar -> showCalendar(true)
+        }
+    }
+
+    private fun showCalendar(visible: Boolean) {
+        if (currentView.value is OnBoardingNavigationView.DateView) {
+            val showCalendar = (currentView.value as OnBoardingNavigationView.DateView).copy(showCalendar = visible)
+            _currentView.value = showCalendar
         }
     }
 
@@ -35,7 +45,7 @@ class OnBoardingViewModel : ViewModel() {
                 _currentView.value = OnBoardingNavigationView.UnitView
             }
             is OnBoardingNavigationView.UnitView -> {
-                _currentView.value = OnBoardingNavigationView.DateView
+                _currentView.value = OnBoardingNavigationView.DateView(showCalendar = false)
             }
             is OnBoardingNavigationView.DateView -> {} // TODO Add actual navigation to Home
         }
@@ -43,20 +53,30 @@ class OnBoardingViewModel : ViewModel() {
 
     private fun setCostPerUnit(cost: Int) {
         _userData.value = _userData.value.copy(costPerUnit = cost)
+        Timber.d("Cost per unit is set to: ${_userData.value.costPerUnit}")
     }
 
     private fun setUnit(units: Int) {
         if (_userData.value.units + units < 0) {
+            Timber.d("Amounts of units is already 0, can't go lower: ${_userData.value.units}")
             return
         }
         _userData.value = _userData.value.copy(units = _userData.value.units + units)
+        Timber.d("Amounts of units is set to: ${_userData.value.units}")
     }
 
-    private fun formatDate(dateWhenQuit: Long) {
-        _userData.value = _userData.value.copy(dateWhenQuit = dateWhenQuit)
-        println(dateWhenQuit)
-        val date1millis = System.currentTimeMillis()
+    private fun setQuitDate(dateWhenQuit: Long) {
+        val daysSinceQuit = OnBoardingHelpers().daysSinceQuit(dateWhenQuit)
 
-        val days = TimeUnit.MILLISECONDS.toDays(date1millis - dateWhenQuit)
+        if (daysSinceQuit < 0) {
+            Timber.d("Trying to set date in the future") // TODO add real error handling, this is checked in UI, but doesn't hurt to double check
+            showCalendar(true)
+            return
+        }
+
+        _userData.value = _userData.value.copy(dateWhenQuit = dateWhenQuit)
+
+        Timber.d("Date when quit is set to: ${_userData.value.dateWhenQuit}")
+        Timber.d("Days since quit is: $daysSinceQuit")
     }
 }
