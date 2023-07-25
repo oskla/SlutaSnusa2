aa76369d4759c9ce4fb789102000bbc650501c1d
Oskar Larsson <90769758+oskla@users.noreply.github.com>
2023-07-25 14:03:34 +0200

    Update contract

--- app/src/main/java/com/antisnusbolaget/slutasnusa2/viewmodel/interface/OnBoardingContract.kt
+++ app/src/main/java/com/antisnusbolaget/slutasnusa2/viewmodel/interface/OnBoardingContract.kt
@@ -1,16 +1,25 @@
 package com.antisnusbolaget.slutasnusa2.viewmodel.`interface` // ktlint-disable package-name
 
 sealed interface OnBoardingEvent {
+    // Cost view
     data class SetCost(val cost: Int) : OnBoardingEvent
+
+    // Unit view
     data class SetUnit(val unitAmount: Int) : OnBoardingEvent
+
+    // Date view
     data class SetDate(val date: Long) : OnBoardingEvent
+    object OpenCalendar : OnBoardingEvent
+    object DismissCalendar : OnBoardingEvent
+
+    // All views
     object NavigateToNextView : OnBoardingEvent
 }
 
 sealed interface OnBoardingNavigationView {
     object CostView : OnBoardingNavigationView
     object UnitView : OnBoardingNavigationView
-    object DateView : OnBoardingNavigationView
+    data class DateView(val showCalendar: Boolean) : OnBoardingNavigationView
 }
 
 data class UserData(
