32aca7ce2895447a9e80e3036418e1e38cec8b00
Oskar Larsson <90769758+oskla@users.noreply.github.com>
2023-07-26 10:16:48 +0200

    TODO

--- app/src/main/java/com/antisnusbolaget/slutasnusa2/ui/screens/onboardingscreen/OnBoardingHelpers.kt
+++ app/src/main/java/com/antisnusbolaget/slutasnusa2/ui/screens/onboardingscreen/OnBoardingHelpers.kt
@@ -2,6 +2,7 @@ package com.antisnusbolaget.slutasnusa2.ui.screens.onboardingscreen
 
 import java.util.concurrent.TimeUnit
 
+// TODO will probably have some sort of DateHandler/DateProvider in the future which will handle all logic connected to Dates.
 class OnBoardingHelpers() {
     private val currentDate = System.currentTimeMillis()
 
