fe186ed29d524d50c11d497f489f21a2a6847a0d
Oskar Larsson <90769758+oskla@users.noreply.github.com>
2023-07-29 20:16:58 +0200

    Start building new UI for date screen

--- app/src/main/java/com/antisnusbolaget/slutasnusa2/ui/screens/onboardingscreen/DateScreen.kt
+++ app/src/main/java/com/antisnusbolaget/slutasnusa2/ui/screens/onboardingscreen/DateScreen.kt
@@ -2,26 +2,44 @@ package com.antisnusbolaget.slutasnusa2.ui.screens.onboardingscreen
 
 import android.content.res.Configuration
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.border
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.antisnusbolaget.slutasnusa2.ui.components.Calendar
 import com.antisnusbolaget.slutasnusa2.ui.components.TextBold
+import com.antisnusbolaget.slutasnusa2.ui.theme.black
+import com.antisnusbolaget.slutasnusa2.ui.theme.yellow
 
 @Composable
 fun DateScreen(
@@ -43,7 +61,7 @@ fun DateScreen(
             verticalArrangement = Arrangement.SpaceEvenly,
         ) {
             TextBold(
-                text = "Slutade du snusa idag?", // TODO stringResources
+                text = "När slutade du snusa?", // TODO stringResources
                 textAlign = TextAlign.Center,
             )
 
@@ -52,24 +70,13 @@ fun DateScreen(
                 verticalArrangement = Arrangement.Center,
                 modifier = Modifier,
             ) {
-                TextBold(
-                    modifier = Modifier.clickable {
-                        println("Pick todays date") // TODO add functionality for picking today's date
-                    }, // TODO stringResources
-                    text = "Ja", // TODO stringResources
-                    textAlign = TextAlign.Center,
-                    fontSize = 100.sp,
-                )
+                RectangleButton(filled = true, buttonLabel = "Välj datum", onClick = openCalendar)
+
                 Spacer(Modifier.height(16.dp))
-                TextBold(
-                    modifier = Modifier.clickable {
-                        openCalendar.invoke()
-                        println("Open calender")
-                    }, // TODO stringResources
-                    text = "Nej", // TODO stringResources
-                    textAlign = TextAlign.Center,
-                    fontSize = 100.sp,
-                )
+
+                RectangleButton(filled = false, buttonLabel = "Idag", onClick = {
+                    onDateSelected(System.currentTimeMillis())
+                })
             }
         }
         if (showCalendar) {
@@ -81,6 +88,63 @@ fun DateScreen(
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun RectangleButton(
+    filled: Boolean,
+    fontSize: TextUnit = 28.sp,
+    buttonLabel: String,
+    onClick: () -> Unit,
+) {
+    val buttonColor = remember { mutableStateOf(if (filled) black else yellow) }
+    val textColor = remember { mutableStateOf(if (filled) yellow else black) }
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .wrapContentHeight()
+            .border(8.dp, black)
+            .background(buttonColor.value)
+            .fillMaxWidth(1f)
+            .combinedClickable(
+                interactionSource = remember {
+                    MutableInteractionSource()
+                },
+                indication = null,
+            ) { }
+            .pointerInput(Unit) {
+                awaitEachGesture {
+                    awaitFirstDown()
+                    // TODO also do scale down
+                    buttonColor.value = if (filled) yellow else black
+                    textColor.value = if (filled) black else yellow
+                    do {
+                        val event = awaitPointerEvent()
+                    } while (event.changes.any { it.pressed })
+                    onClick.invoke()
+                    textColor.value = if (filled) yellow else black
+                    buttonColor.value = if (filled) black else yellow
+                }
+            }
+            .padding(vertical = 32.dp, horizontal = 24.dp),
+    ) {
+        TextBold(
+            modifier = Modifier,
+            text = buttonLabel,
+            textAlign = TextAlign.Center,
+            fontSize = fontSize,
+            color = textColor.value,
+        )
+        Icon(
+            modifier = Modifier.size(42.dp),
+            imageVector = Icons.Default.ArrowForward,
+            contentDescription = "button",
+            tint = textColor.value,
+        )
+    }
+}
+
 private const val componentName = "DateScreen"
 
 @Preview("$componentName (light)", showBackground = true)
@@ -89,5 +153,11 @@ private const val componentName = "DateScreen"
 @Preview("$componentName (large screen)", device = Devices.PIXEL_C)
 @Composable
 private fun PreviewComponent() {
-    // DateScreen()
+    DateScreen(
+        openCalendar = {},
+        dismissCalendar = {},
+        onDateSelected = {},
+        onBackPressed = {},
+        showCalendar = false,
+    )
 }
