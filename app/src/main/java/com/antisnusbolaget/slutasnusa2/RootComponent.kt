dee017c1bf6c9bf73e0a5b01cfaa15556256d5e0
nasvalljohan <j.nasvall95@gmail.com>
2023-08-01 21:06:41 +0200

    Removed scaffold Add placeholder boolean

--- app/src/main/java/com/antisnusbolaget/slutasnusa2/RootComponent.kt
+++ app/src/main/java/com/antisnusbolaget/slutasnusa2/RootComponent.kt
@@ -3,37 +3,24 @@ package com.antisnusbolaget.slutasnusa2
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.antisnusbolaget.slutasnusa2.navigation.NavGraph
-import com.antisnusbolaget.slutasnusa2.navigation.Screen
-import com.antisnusbolaget.slutasnusa2.ui.components.TopBar
+import com.antisnusbolaget.slutasnusa2.navigation.OnBoardingNavGraph
+import com.antisnusbolaget.slutasnusa2.ui.screens.mainscreen.MainScreen
 
 @Composable
 fun RootComponent() {
     val navController = rememberNavController()
-    val topBarState = rememberSaveable { mutableStateOf(false) }
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
 
-    topBarState.value = Screen.shouldShowTopBar(navBackStackEntry?.destination?.route)
+    val isUserDataAvailable = true // TODO Check for real
 
     Box(modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background)) {
-        Scaffold(
-            content = { scaffoldPadding ->
-                Box(modifier = Modifier.padding(paddingValues = scaffoldPadding)) {
-                    NavGraph(navController = navController)
-                }
-            },
-            bottomBar = { /* TODO */ },
-            topBar = { TopBar(showTopBar = topBarState.value) },
-        )
+        if (isUserDataAvailable) {
+            MainScreen(navController = navController)
+        } else {
+            OnBoardingNavGraph(navController = navController)
+        }
     }
 }
