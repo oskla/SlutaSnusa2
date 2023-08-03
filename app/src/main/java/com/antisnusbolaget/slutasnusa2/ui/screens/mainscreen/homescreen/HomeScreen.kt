a628eb7dbbefd1e40f0553513caf8f055e6de0a8
nasvalljohan <j.nasvall95@gmail.com>
2023-08-03 17:38:07 +0200

    For some reason data is not showing, only in log

--- app/src/main/java/com/antisnusbolaget/slutasnusa2/ui/screens/mainscreen/homescreen/HomeScreen.kt
+++ app/src/main/java/com/antisnusbolaget/slutasnusa2/ui/screens/mainscreen/homescreen/HomeScreen.kt
@@ -1,12 +1,17 @@
 package com.antisnusbolaget.slutasnusa2.ui.screens.mainscreen.homescreen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,7 +21,9 @@ import com.antisnusbolaget.slutasnusa2.navigation.Screen
 import com.antisnusbolaget.slutasnusa2.ui.components.TextBold
 import com.antisnusbolaget.slutasnusa2.viewmodel.LoadingState
 import com.antisnusbolaget.slutasnusa2.viewmodel.homescreen.HomeScreenViewModel
+import com.antisnusbolaget.slutasnusa2.viewmodel.homescreen.HomeState
 import org.koin.androidx.compose.koinViewModel
+import timber.log.Timber
 
 @Composable
 fun HomeScreen(
@@ -39,9 +46,23 @@ fun HomeScreen(
                 CircularProgressIndicator()
             }
             LoadingState.SUCCESS -> {
-                TextBold(text = "HAJ HAJ") // TODO HomeContent
+                HomeScreenContent(uiState = uiState)
             }
         }
+    }
+}
+
+@Composable
+fun HomeScreenContent(uiState: State<HomeState>) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        TextBold(
+            modifier = Modifier.clickable { Timber.d("Osk, ${uiState.value.userData.costPerUnit}") },
+            text = "${uiState.value.userData.costPerUnit}",
+        )
     }
 }
 
